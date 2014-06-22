a9d3271e45f1c492bde6165b9a246ea2e48205ce
Ryan Gerry <gerryster@gmail.com>
2014-06-22 15:33:29 -0500

    Removing obsolete comments.

--- bin/process_accounts.js
+++ bin/process_accounts.js
@@ -6,13 +6,11 @@
   var fs = require('fs');
   var inputFile = process.argv[2];
 
-  //Reading file test.txt
   fs.readFile(inputFile,'utf8',function(err,data){
     if(err) {
       console.error("Could not open file %s: %s", inputFile, err);
       process.exit(1);
    }
-   //Calling replacement function
    reader = new AccountsReader(data);
    console.log("accounts: \n" + reader.accounts().join("\n") + "\n");
   });
