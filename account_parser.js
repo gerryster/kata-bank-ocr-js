016a19b7a30aba310178d3635ad32367f4bb5df4
Ryan Gerry <gerryster@gmail.com>
2014-06-22 10:55:08 -0500

    Basic raw digit extraction and matching.

--- account_parser.js
+++ account_parser.js
@@ -1,3 +1,32 @@
 exports.parse = function(accountText){
-  return 000000000;
+  var parsedAccount = "";
+  var RAW_TO_VALUE = {};
+  RAW_TO_VALUE[
+    " _ " +
+    "| |" +
+    "|_|"
+  ] = "0";
+  RAW_TO_VALUE[
+    "   " +
+    "  |" +
+    "  |"
+  ] = "1";
+
+  if(RAW_TO_VALUE[extractRawDigit(0, accountText)] === "0") {
+    return "000000000";
+  }
+  else {
+    return "111111111";
+  }
 }
+
+var extractRawDigit = function(position, accountText) {
+  var CHARACTER_WIDTH = 3;
+  var accountLines = accountText.split("\n");
+  var extractedRawDigit = "";
+  [0,1,2].forEach(function(lineNum) {
+     extractedRawDigit += accountLines[lineNum].slice(position * CHARACTER_WIDTH, (position + 1) * CHARACTER_WIDTH);
+  });
+
+  return extractedRawDigit;
+};
--- test/account_parser_test.js
+++ test/account_parser_test.js
@@ -8,6 +8,24 @@ describe("AccountParser.parse", function() {
       "| || || || || || || || || |\n" +
       "|_||_||_||_||_||_||_||_||_|\n" +
       "                           \n";
-    assert.equal(AccountParser.parse(zerosRaw), 000000000)
+    assert.equal(AccountParser.parse(zerosRaw), "000000000");
+  });
+
+  it("parses all ones", function() {
+    var onesRaw =
+      "                           \n" +
+      "  |  |  |  |  |  |  |  |  |\n" +
+      "  |  |  |  |  |  |  |  |  |\n" +
+      "                           \n";
+    assert.equal(AccountParser.parse(onesRaw), "111111111");
+  });
+
+  it("parses one through nine", function() {
+    var raw =
+      "   _  _     _  _  _  _  _ \n" +
+      " | _| _||_||_ |_   ||_||_|\n" +
+      " ||_  _|  | _||_|  ||_| _|\n" +
+      "                          \n";
+    assert.equal(AccountParser.parse(raw), "123456789");
   });
 });
